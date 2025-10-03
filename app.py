f1ecd1023086e28f255bbe621a6a95748b586e0f
tejasgund <gundtejas8@gmail.com>
2025-10-03 11:12:28 +0530

    created app.py

--- app.py
+++ app.py
@@ -16,17 +16,26 @@ def api_data():
         "message": "Hello from Flask API!",
         "status": "success"
     })
-@app.route("/api/v1/customer/{car_number}")
+
+@app.route("/api/v1/customer/<car_number>")
 def api_customer(car_number):
-    return {
-  "status": "success",
-  "data": {
-    "is_old_customer": true,
-    "customer_id": 101,
-    "name": "Jane Doe",
-    "phone": "9876543210"
-  }
-}
+    if car_number == "MH13CL3290":
+        return jsonify({
+            "status": "success",
+            "data": {
+                "is_old_customer": True,
+                "customer_id": 101,
+                "name": "Jane Doe",
+                "phone": "9876543210"
+            }
+        })
+    else:
+        # Return empty data if car_number not found
+        return jsonify({
+            "status": "success",
+            "data": {}
+        })
+
 
 
 if __name__ == "__main__":
