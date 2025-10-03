510be6b69ee3669356fdea9b075655c79fccf394
tejasgund <gundtejas8@gmail.com>
2025-10-03 23:38:01 +0530

    created app.py

--- app.py
+++ app.py
@@ -10,64 +10,7 @@ def home():
     return send_from_directory("frontend", "index.html")
 
 # Example API route
-@app.route("/api/data")
-def api_data():
-    return jsonify({
-        "message": "Hello from Flask API!",
-        "status": "success"
-    })
 
-@app.route("/api/v1/customer/<car_number>",methods=["GET"])
-def api_customer(car_number):
-    if car_number == "MH13CL3290":
-        return jsonify({
-            "status": "success",
-            "data": {
-                "is_old_customer": True,
-                "customer_id": 101,
-                "name": "Jane Doe",
-                "phone": "9876543210"
-            }
-        })
-    else:
-        # Return empty data if car_number not found
-        return jsonify({
-            "status": "success",
-            "data": {}
-        })
-
-
-@app.route("/api/v1/customer", methods=["POST"])
-def create_customer():
-    data=request.get_json()
-    car_number = data["car_number"]
-    name = data["name"]
-    phone = data["phone"]
-    conn=config.database()
-    cursor=conn.cursor()
-    cursor.execute("SELECT id FROM Customer WHERE car_number=%s",(car_number,))
-    existing_customer=cursor.fetchone()
-    if existing_customer:
-        cursor.close()
-        conn.close()
-        return jsonify({
-            "status": "fail",
-            "message": "This customer already exists."
-        }),400
-    else:
-        cursor.execute(
-            "INSERT INTO Customer(car_number, name, phone) VALUES (%s, %s, %s)",(car_number, name, phone)
-        )
-        conn.commit()
-        customer_id=cursor.lastrowid
-        cursor.close()
-        conn.close()
-
-        return jsonify({
-            "status": "success",
-            "message": "Customer created successfully.",
-            "customer_id": customer_id
-        }),201
 
 
 
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def database():
     host="15.206.151.116",
     user="admin",
     passwd="admin",
-    database="Sahyadri",
+    database="sahyadri_Washing_center",
     port=3306
 )
 
