c8ccaf9e9b4b1175ebf4f74516a92c7f821c47c3
tejasgund <gundtejas8@gmail.com>
2025-10-06 16:18:30 +0530

    added message sent test from twilio

--- api_functions.py
+++ api_functions.py
@@ -232,7 +232,6 @@ def create_bill(customerName, mobileNumber, vehicleNumber, vehicleType, services
     finally:
         cursor.close()
         conn.close()
-        generate_bill_message(int(1005))
 
 
 def stats():
