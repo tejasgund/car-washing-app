8c6f6e79553656f6b38392ea9a0378891009f3f6
tejasgund <gundtejas8@gmail.com>
2025-10-10 00:37:47 +0530

    added index2.html

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import pymysql
+
 def database():
     return pymysql.connect(
-    host="172.19.0.2",
-    user="admin",
-    passwd="admin",
-    database="car_washing_app_prod",
-    port=3306
-)
-
-
+        # CHANGE: Use the container NAME as the HOSTNAME
+        host="mysql_prod",
+        user="admin",
+        passwd="admin",
+        database="car_washing_app_prod",
+        port=3306
+    )
