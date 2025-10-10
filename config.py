99e0d4277a1656525b0ab370687dfc29bd47f464
tejasgund <gundtejas8@gmail.com>
2025-10-10 19:13:01 +0530

    added index2.html

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import pymysql
 def database():
     return pymysql.connect(
-    host="15.206.151.116",
+    host="15.206.90.104",
     user="admin",
     passwd="admin",
     database="car_washing_app_qa",
