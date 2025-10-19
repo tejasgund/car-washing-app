b4fc653656e471576519e569bf6217b75adb347e
tejasgund <gundtejas8@gmail.com>
2025-10-19 12:22:02 +0530

    Changes name in marathi sahyadri

--- messenger.py
+++ messenger.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 
 def send_message(number,message):
     account_sid = 'AC2cf2999e6243491028eef6eede3d6fc1'
-    auth_token = '53d76e0024a88f886d0563bef106df6c'
+    auth_token = '4296fc6b8dd850cbf3a7dbbbdf26f866'
     client = Client(account_sid, auth_token)
 
 
@@ -12,8 +12,11 @@ def send_message(number,message):
             body=message,
             to=f'whatsapp:+91{number}'
         )
+        return {f"message_id":message.sid,
+                "status":"success"}
 
     except Exception as e:
         return {f"Failed to send message : {e}"}
 
 
+print(send_message(8177809890,"test"))
