cc06ba063161b31fe382695dfb0725c4689aa907
tejasgund <gundtejas8@gmail.com>
2026-03-13 16:50:08 +0530

    commit craeted env varibles for db and added the logs

--- config.py
+++ config.py
@@ -1,9 +1,41 @@
 import pymysql
+import os
+from AppLog.applog import get_logger
+
 def database():
-    return pymysql.connect(
-    host="3.109.100.60",
-    user="admin",
-    passwd="admin",
-    database="car_washing_app_prod",
+    log = get_logger("DatabaseConnection")
+    host=os.environ.get('MYSQL_HOST')
+    user=os.environ.get('MYSQL_USER')
+    passwd=os.environ.get('MYSQL_PASSWORD')
+    database=os.environ.get('MYSQL_DATABASE')
     port=3306
-)
+    if not host:
+        log.error("MYSQL_HOST environment variable not set")
+    if not user:
+        log.error("MYSQL_USER environment variable not set")
+    if not passwd:
+        log.error("MYSQL_PASSWORD environment variable not set")
+    if not database:
+        log.error("MYSQL_DATABASE environment variable not set")
+    if not port:
+        log.error("MYSQL_PORT environment variable not set")
+
+
+    try:
+        conn = pymysql.connect(
+        host=host,
+        user=user,
+        passwd=passwd,
+        database=database,
+        port=port
+        #host="3.109.100.60",
+        #user="admin",
+        #passwd="admin",
+        #database="car_washing_app_prod",
+        #port=3306
+        )
+        log.info("MYSQL_DATABASE environment variable set to {}".format(database))
+        return conn
+    except Exception as e:
+        log.error(f"Database connection failed: {str(e)}")
+
